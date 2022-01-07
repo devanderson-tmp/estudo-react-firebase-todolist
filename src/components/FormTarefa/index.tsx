571780abe9a6fcbddc7b08b01fd260759ab37a13
Anderson Pires <devanderson.pires@gmail.com>
2022-01-06 21:05:37 -0300

    :sparkles: adicionar validação no form tarefa
    validando time picker

--- src/components/FormTarefa/index.tsx
+++ src/components/FormTarefa/index.tsx
@@ -1,20 +1,35 @@
 import {useFormik} from 'formik';
 import * as yup from 'yup';
 import {Box, Button, TextField} from '@mui/material';
+import DateAdapter from '@mui/lab/AdapterLuxon';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import TimePicker from '@mui/lab/TimePicker';
+import {DateTime} from 'luxon';
 
 const validationSchema = yup.object({
 	tarefa: yup.string().required('Tarefa é obrigatório'),
-	tempo: yup.string().required('Tempo é obrigatório'),
+	tempo: yup
+		.date()
+		.typeError('Tempo inválido')
+		.min(
+			DateTime.local().set({hour: 0, minute: 4, second: 59}),
+			'O tempo mínimo é de 00:05:00'
+		)
+		.max(
+			DateTime.local().set({hour: 1, minute: 30, second: 0}),
+			'O tempo máximo é de 01:30:00'
+		),
 });
 
 function FormTarefa() {
 	const formik = useFormik({
 		initialValues: {
 			tarefa: '',
-			tempo: '00:30:00',
+			tempo: DateTime.local().set({hour: 0, minute: 5, second: 0}),
 		},
 		validationSchema: validationSchema,
-		onSubmit: values => console.log(values),
+		onSubmit: values =>
+			console.log(values.tempo.hour, values.tempo.minute, values.tempo.second),
 	});
 
 	return (
@@ -34,18 +49,26 @@ function FormTarefa() {
 					placeholder="O que você quer estudar?"
 				/>
 
-				<TextField
-					type="time"
-					name="tempo"
-					id="tempo"
-					onBlur={formik.handleBlur}
-					onChange={formik.handleChange}
-					error={formik.touched.tempo && Boolean(formik.errors.tempo)}
-					helperText={formik.touched.tempo && formik.errors.tempo}
-					value={formik.values.tempo}
-					label="Tempo"
-					variant="standard"
-				/>
+				<LocalizationProvider dateAdapter={DateAdapter}>
+					<TimePicker
+						ampm={false}
+						openTo="hours"
+						views={['hours', 'minutes', 'seconds']}
+						inputFormat="HH:mm:ss"
+						mask="__:__:__"
+						onChange={value => formik.setFieldValue('tempo', value)}
+						value={formik.values.tempo}
+						label="Tempo"
+						renderInput={params => (
+							<TextField
+								{...params}
+								error={formik.touched.tempo && Boolean(formik.errors.tempo)}
+								helperText={formik.touched.tempo && formik.errors.tempo}
+								variant="standard"
+							/>
+						)}
+					/>
+				</LocalizationProvider>
 			</Box>
 
 			<Button variant="contained" type="submit">
